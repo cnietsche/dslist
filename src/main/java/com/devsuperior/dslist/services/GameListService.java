3a9773b0c378e3b3d24970f3c43445f1bd00cffc
Christian Nietsche <christiannietsche@hotmail.com>
2025-01-21 21:22:36 -0300

    GameList: Repository, Service, Controller and DTO

---
+++ src/main/java/com/devsuperior/dslist/services/GameListService.java
@@ -0,0 +1,26 @@
+package com.devsuperior.dslist.services;
+
+import com.devsuperior.dslist.dto.GameListDTO;
+import com.devsuperior.dslist.repositories.GameListRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class GameListService {
+
+    @Autowired
+    private GameListRepository gameListRepository;
+
+    @Transactional(readOnly = true)
+    public List<GameListDTO> findAll() {
+        return gameListRepository.findAll().stream().map(x -> new GameListDTO(x)).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public GameListDTO findById(Long id) {
+        return new GameListDTO(gameListRepository.findById(id).get());
+    }
+}
